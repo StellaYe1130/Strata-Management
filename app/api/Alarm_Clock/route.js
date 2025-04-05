0755ca8b5f501c73c709b089abf7d23d622706db
Ye <ye@YedeMacBook-Air.local>
2025-04-05 12:39:05 +1100

    cron

---
+++ app/api/Alarm_Clock/route.js
@@ -0,0 +1,15 @@
+export const config = {
+    runtime: 'edge',
+  };
+  export async function GET() {
+    const reminderMessage = {
+      date: new Date().toISOString(),
+      message: "Reminder: Check if insurance payment reminder needs to be sent.",
+    };
+    return new Response(JSON.stringify(reminderMessage), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  
