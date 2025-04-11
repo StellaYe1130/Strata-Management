1ecfed7f629edf78b20db528addfd6ba58b5ec48
Ye <xinyanye2004@gmail.com>
2025-04-11 23:03:29 +1000

    Thankyou

--- app/thank_you/page.js
+++ app/thank_you/page.js
@@ -1,8 +1,8 @@
 export default function ThankYouPage() {
     return (
-      <div style={{ padding: "2rem" }}>
-        <h1>Thank you for contacting us!</h1>
-        <p>We have received your message and will get back to you soon.</p>
+      <div>
+        <h1>Thank you for your message!</h1>
+        <p>We will get back to you shortly.</p>
       </div>
     );
   }
