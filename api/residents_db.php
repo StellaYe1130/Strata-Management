9426964b16b70164f49cd56000da21cbdb6bd0de
Ye <xinyanye2004@gmail.com>
2025-04-28 01:28:36 +1000

    super

--- api/residents_db.php
+++ api/residents_db.php
@@ -1,28 +1,20 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "720608Sy"; 
-$dbname = "strata_management";
+$url = 'https://pxepetpoaobjzazhbxsb.supabase.co/rest/v1/residents?select=*';
+$apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4ZXBldHBvYW9ianphemhieHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3NjU1OTYsImV4cCI6MjA2MTM0MTU5Nn0.tDWJbGJA7A2xBw6-8fxY6tU1zP9uH7MUOZy9-4c_sZU';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'apikey: ' . $apiKey,
+    'Authorization: Bearer ' . $apiKey,
+    'Content-Type: application/json'
+]);
 
-$sql = "SELECT * FROM residents";
-$result = $conn->query($sql);
-
-$residents = [];
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $residents[] = $row;
-    }
-}
+$response = curl_exec($ch);
+curl_close($ch);
 
 header('Content-Type: application/json');
-echo json_encode($residents);
-
-$conn->close();
+echo $response;
 ?>
