bbb956e728aaafd62c4ae746715860e7085edf77
Ye <xinyanye2004@gmail.com>
2025-04-28 16:31:02 +1000

    logout1.0

--- app/page.js
+++ app/page.js
@@ -21,8 +21,17 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       {username && (
-         <h1 className="text-2xl font-bold mb-4">Welcome back, {username}!</h1>
+      <>
+        <h1 className="text-2xl font-bold mb-4">Welcome back, {username}!</h1>
+        <button 
+          onClick={handleLogout}
+          className="bg-red-500 text-white px-4 py-2 rounded mb-6"
+        >
+          Logout
+        </button>
+        </>
       )}
+
         <Image
           className="dark:invert"
           src="/next.svg"
