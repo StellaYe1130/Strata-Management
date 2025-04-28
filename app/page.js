a9e2504bc689743743d8be1475b73df03aeccc50
Ye <xinyanye2004@gmail.com>
2025-04-28 16:18:33 +1000

    connectHOme

--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    const cookies = document.cookie.split("; ");
+    const userCookie = cookies.find(row => row.startsWith("username="));
+    if (userCookie) {
+      setUsername(userCookie.split("=")[1]);
+    }
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      {username && (
+         <h1 className="text-2xl font-bold mb-4">Welcome back, {username}!</h1>
+      )}
         <Image
           className="dark:invert"
           src="/next.svg"
