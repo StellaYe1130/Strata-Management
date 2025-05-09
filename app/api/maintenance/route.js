424102146a8b6be3d99e0253a312738127fe977f
Ye <xinyanye2004@gmail.com>
2025-05-09 18:31:50 +1000

    maintenance

--- app/api/maintenance/route.js
+++ app/api/maintenance/route.js
@@ -9,7 +9,7 @@ export const runtime = "edge";
 
 export async function GET() {
   const { data, error } = await supabase
-    .from("Maintenance")
+    .from('"Maintenance"')
     .select("*");
 
   if (error) {
